8b8129618c0f7278655b9fee6e1e7f543a3fcf3d
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-30 15:34:07 -0500

    init project

---
+++ app/src/main/java/com/loschimbitas/icm_taller2_loschimbitas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.loschimbitas.icm_taller2_loschimbitas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
