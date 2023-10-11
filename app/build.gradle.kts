9b9391ddf9fa4dd217abc3d41c83e5072dbd2c8d
davidfer1112 <davidfernando1112@gmail.com>
2023-10-11 11:19:55 -0500

    Feature: carga mapa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation ("org.osmdroid:osmdroid-android:6.1.14")
+    implementation ("org.mapsforge:mapsforge-map-android:0.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/loschimbitas/icm_taller2_loschimbitas/Rutas.kt
+++ app/src/main/java/com/loschimbitas/icm_taller2_loschimbitas/Rutas.kt
@@ -1,11 +1,47 @@
 package com.loschimbitas.icm_taller2_loschimbitas
 
+import android.content.res.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.model.Tile
+import com.loschimbitas.icm_taller2_loschimbitas.databinding.ActivityRutasBinding
+import org.osmdroid.api.IMapController
+import org.osmdroid.library.BuildConfig
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+
 
 class Rutas : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityRutasBinding
+    private val latitude = 4.62
+    private val longitude = -74.07
+    private val startPoint = GeoPoint(latitude,longitude)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rutas)
+
+        Configuration.getInstance().setUserAgentValue(BuildConfig.BUILD_TYPE)
+
+        binding = ActivityRutasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.osmMap.setTileSource(TileSourceFactory.MAPNIK)
+        binding.osmMap.setMultiTouchControls(true)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.osmMap.onResume()
+        val mapController: IMapController = binding.osmMap.controller
+        mapController.setZoom(18.0)
+        mapController.setCenter(this.startPoint)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.osmMap.onPause()
+    }
+
 }
