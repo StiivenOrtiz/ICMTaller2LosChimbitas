512bcc0537c6d5d642be08f0a8cc15f5a0289fb2
davidfer1112 <davidfernando1112@gmail.com>
2023-10-11 12:15:45 -0500

    Feature: marca

--- app/src/main/java/com/loschimbitas/icm_taller2_loschimbitas/Rutas.kt
+++ app/src/main/java/com/loschimbitas/icm_taller2_loschimbitas/Rutas.kt
@@ -1,5 +1,7 @@
 package com.loschimbitas.icm_taller2_loschimbitas
 
+import android.app.UiModeManager
+import android.content.Context
 import android.content.res.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +12,8 @@ import org.osmdroid.library.BuildConfig
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.TilesOverlay
 
 
 class Rutas : AppCompatActivity() {
@@ -29,6 +33,15 @@ class Rutas : AppCompatActivity() {
         binding.osmMap.setTileSource(TileSourceFactory.MAPNIK)
         binding.osmMap.setMultiTouchControls(true)
 
+        val markrPoint = GeoPoint(4.62,-74.07)
+        val marker = Marker(binding.osmMap)
+        marker.title = "Mi marca uwu"
+        val myIcon = resources.getDrawable(org.osmdroid.library.R.drawable.marker_default,theme)
+        marker.icon = myIcon
+        marker.position = markrPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        binding.osmMap.overlays.add(marker)
+
     }
 
     override fun onResume() {
@@ -37,6 +50,18 @@ class Rutas : AppCompatActivity() {
         val mapController: IMapController = binding.osmMap.controller
         mapController.setZoom(18.0)
         mapController.setCenter(this.startPoint)
+
+        //ajuste de tema para mapa segun el modo del dispoditivo
+        val uiManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+        if(uiManager.nightMode == UiModeManager.MODE_NIGHT_YES){
+            binding.osmMap.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+        }
+
+        //para poner el dark Mode de forma constante
+        //binding.osmMap.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+
+
+
     }
 
     override fun onPause() {
